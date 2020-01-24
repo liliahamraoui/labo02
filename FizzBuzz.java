17d7ad7e7f62f091ee5d158af8d8368b74a42218
Lilia <lilia14hamraoui@hotmail.com>
2020-01-24 14:26:24 -0500

    Ajout autre commentaire pertinent

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -3,10 +3,25 @@
 	public class main void 
  		
 		for (int i=0;)
+    {
+             if (i%15 == 0)
+            printf ("FizzBuzz\t");
 
+        else if ((i%3) == 0)
+            printf("Fizz\t");
 
+        else if ((i%5) == 0)
+            printf("Buzz\t");
+
+        else
+            printf("%d\t", i);
+
+    }
+
+   System.out.println("fin normal");
+
+	//fin normal
  
  
  }
 // un autre commentaire
-
